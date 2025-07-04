b1bd8807a4d2eba94a276212347df87213da6934
eddymafin <marumai2451@gmail.com>
2025-07-04 13:22:56 +0900

    プロジェクトファイル追加

---
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.form.ReviewRegistForm;
+
+@Controller
+public class ReviewController {
+	
+	@GetMapping("/show-review-form")
+	public String showReviewForm(@ModelAttribute ReviewRegistForm form) {
+		return "regist-review";
+	}
+	
+	@PostMapping("/regist-review")
+	public String registReview(@ModelAttribute ReviewRegistForm form) {
+		return "confirm-regist-review";
+	}
+	
+	@PostMapping("/show-review-form-ret")
+	public String showReviewFormRet(@ModelAttribute ReviewRegistForm form) {
+		return "regist-review";
+	}
+	
+	@GetMapping("/show-review-form")
+	public String confirmRegistReview() {
+		return "complete-regist-review";
+	}
+
+}
